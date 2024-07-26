da3fb60169b467d4aa952bafa2963992d953ba58
mirunaaf <mirunaaff@gmail.com>
2024-07-26 14:58:09 +0100

    Initial commit

---
+++ web.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import functions
+
+# Declare todos variable
+todos = functions.get_todos()
+
+# Create a function that gets the user input value and adds it in the todo list
+def add_todo():
+    todo_toadd = st.session_state["new_todo"] + "\n"
+    print(todo_toadd)
+    todos.append(todo_toadd)
+    functions.write_todos(todos)
+
+
+# Gets todo list from todos.txt
+todos = functions.get_todos()
+# Return a title instance
+st.title("My Todo App")
+
+# Creates text
+st.write("The app that will increase your productivity.")
+
+for index, todo in enumerate(todos):
+# Creates checkbox with the todos from the todos.txt
+    checkbox = st.checkbox(todo, key=todo) #stores each checkbox in a variable
+    if checkbox:
+        # Remove the todo checked from the list
+        todos.pop(index)
+        functions.write_todos(todos)
+        # Delete the pair selected(from session_state dictionary)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add a new todo:",
+              on_change=add_todo, key='new_todo')
+
+#visualize the updated session_state dictionary
+#st.session_state
